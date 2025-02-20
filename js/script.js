4854fcf7a98d298e8c383a0e80540e9934eedd96
luisfernando80 <lfernando.ti80@gmail.com>
2025-02-20 11:52:48 -0300

    ajustes cabeçalho
    coloquei um padding left no cabeçalho. Pequeno ajuste para melhorar a visualização quando mudar para brackpoint mobile.

--- js/script.js
+++ js/script.js
@@ -2,6 +2,15 @@ const elemHeader = document.querySelector("header");
 const elemMenu = document.querySelector(".js-menu");
 
 elemHeader.addEventListener("click", () => {
-  console.log("clicou");
+  const barras = elemMenu.querySelectorAll("div");
+  const barraCima = barras[0];
+  const barraBaixo = barras[1];
+
   elemHeader.classList.toggle("grid-rows-[3.75rem_1fr]");
+
+  barraCima.classList.toggle("rotate-45");
+  barraCima.classList.toggle("translate-y-[0.28125rem]");
+
+  barraBaixo.classList.toggle("-rotate-45");
+  barraBaixo.classList.toggle("-translate-y-[0.28125rem]");
 });
