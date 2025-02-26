a70f4cf65d47b898ac1e07927c181d3d0eef22f1
luisfernando80 <lfernando.ti80@gmail.com>
2025-02-25 21:15:46 -0300

    Secao Sob Medida
    secao pronta para desktop, faltando fazer responsividade.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["*.html", "js/*.js"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        "subir-rg": {
+          "0%": { transform: "translate(-50%,0)" },
+          "100%": { transform: "translate(-50%,-80%)" },
+        },
+        "subir-dinheiro": {
+          "0%": { transform: "translate(-50%,0)" },
+          "100%": { transform: "translate(-50%,-50%)" },
+        },
+      },
+    },
     fontFamily: {
       avenir: ["Avenir", "sans-serif"],
       avenir_heavy: ["Avenir Heavy", "sans-serif"],
